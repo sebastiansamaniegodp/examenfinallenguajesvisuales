5697b1f821e50a18911ab3988137409ba60a76e3
sebastiansamaniegodp <138633176+sebastiansamaniegodp@users.noreply.github.com>
2023-07-04 21:04:55 -0400

    Agregar backup de base de datos

--- Uninorte/SysWpfUninorte/Properties/Settings.Designer.cs
+++ Uninorte/SysWpfUninorte/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace SysWpfUninorte.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,17 @@ namespace SysWpfUninorte.Properties {
         public string uninorteConnectionString {
             get {
                 return ((string)(this["uninorteConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=SEBASW11\\SQLEXPRESS;Initial Catalog=UninorteStore;Persist Security In" +
+            "fo=True;User ID=sa;Password=12345")]
+        public string UninorteStoreConnectionString {
+            get {
+                return ((string)(this["UninorteStoreConnectionString"]));
             }
         }
     }
